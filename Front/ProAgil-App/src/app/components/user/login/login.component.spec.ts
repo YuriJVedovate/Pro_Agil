6f18b1b72bd730bd44c7aed45382e8be56f575d0
Yuri Vedovate <tr029513@minhati.com.br>
2022-01-03 22:05:52 -0300

    teste unitario login

--- Front/ProAgil-App/src/app/components/user/login/login.component.spec.ts
+++ Front/ProAgil-App/src/app/components/user/login/login.component.spec.ts
@@ -1,9 +1,13 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '@app/services/auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+
+class ToastrServiceMock {}
+class AuthServiceMock {}
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -11,7 +15,21 @@ describe('LoginComponent', () => {
 
   beforeEach(async() => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      providers: [
+        { 
+          provide: AuthService,
+          useClass: AuthServiceMock
+        },
+        {
+          provide: ToastrService,
+          useClass: ToastrServiceMock
+        },
+      ],
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+      ],
     })
     .compileComponents();
   });
@@ -20,6 +38,10 @@ describe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 
 });
