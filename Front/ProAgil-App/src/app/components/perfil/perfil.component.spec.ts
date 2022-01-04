52e742c1761918d951106a31148545f8627e2d33
Yuri Vedovate <tr029513@minhati.com.br>
2022-01-03 22:23:58 -0300

    base teste unitario perfil

--- Front/ProAgil-App/src/app/components/perfil/perfil.component.spec.ts
+++ Front/ProAgil-App/src/app/components/perfil/perfil.component.spec.ts
@@ -1,6 +1,17 @@
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormBuilder } from '@angular/forms';
 import { PerfilComponent } from './perfil.component';
+
+@Component({
+  selector: 'app-titulo',
+  template: '',
+})
+export class TituloComponentMock {
+  @Input() titulo: string = 'Editar Evento';
+  @Input() iconClass: string = 'fa fa-user';
+  @Input() subtitulo: string = ' Desde 2021';
+}
 
 describe('PerfilComponent', () => {
   let component: PerfilComponent;
@@ -8,7 +19,10 @@ describe('PerfilComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PerfilComponent ]
+      declarations: [ PerfilComponent, TituloComponentMock ],
+      providers: [
+        FormBuilder
+      ],
     })
     .compileComponents();
   });
@@ -17,6 +31,10 @@ describe('PerfilComponent', () => {
     fixture = TestBed.createComponent(PerfilComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 
 });
